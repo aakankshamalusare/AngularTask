0485598692b780f61996475186f25135aa9ee0fc
aakanksha22 <akankshamalusare@gmail.com>
2023-09-10 20:54:00 +0530

    Operators

---
+++ operators.ts
@@ -0,0 +1,13 @@
+/*
+    Operator in Typescript
+
+    1.Arithmathic => +,-,*,/,%
+    2.logical => &&,||,
+    3.bitwise => &,|,^,!,~,<<,>>
+    4.assignment => =,+=,-=,*=,/=,%=
+    5.relation => >,<,>=,<=,!=,== (It checks only value ), === (strongly equality => it checkas value as well as type )
+    6.ternary => condition?expression1:expression2
+    7. unary => ++,--,post/pre ++a,a--
+
+
+*/
